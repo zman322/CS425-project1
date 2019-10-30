cbb63f5683492875976109776ce24e3d4d1a264b
Zeth Malcom <zmalcom@stu.jsu.edu>
2019-10-30 13:20:36 -0500

    10/30/19

--- web/scripts/Project1.js
+++ web/scripts/Project1.js
@@ -5,3 +5,69 @@
  */
 
 
+var Project1 = ( function() {
+
+    return {
+
+        init: function() {
+            
+            $("#version").html( "jQuery Version: " + $().jquery );
+
+        },
+        
+        submitSessionForm: function() {
+
+            var sessionID = $("#sessionid").val();
+   
+            $.ajax({
+
+                url: 'registration?ID=' + sessionID,
+                method: 'GET',
+                data: $('#searchform').serialize(),
+
+                success: function(response) {
+
+                    $("#table").html(response);
+
+                }
+
+            });
+
+            return false;
+
+        },
+        
+        submitNewAttendeeForm: function() {
+
+            var firstname = $("#firstname").val();
+            var lastname = $("#lastname").val();
+            var displayname = $("#displayname").val();
+            var sessionID = $("#sessionid").val();
+            
+            var dataForm{
+                firstname: firstname,
+                lastname: lastname,
+                displayname: displayname,
+                sessionID: sessionid
+            }
+            
+            $.ajax({
+
+                url: 'registration',
+                method: 'POST',
+                data: dataForm,
+                dataType: "json",
+
+                success: function(response) {
+
+                    $("#responseArea").html("Congratulations! You have successfully registered as: " + response.displayname + "<br> <br>Your registration code is: <strong>" + response.code +"</strong>");
+
+                }
+
+            });
+
+        }
+
+    };
+
+}());
